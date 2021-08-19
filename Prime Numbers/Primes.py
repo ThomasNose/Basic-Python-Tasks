dc5a8a83df8313a4005af677b3177808d1e72fcd
ThomasNose <thomasadugdale@hotmail.com>
2021-08-19 16:37:10 +0100

    Update Primes.py

--- Prime Numbers/Primes.py
+++ Prime Numbers/Primes.py
@@ -1,5 +1,3 @@
-import math
-
 for i in range(0,3000): # 3000 is the maximum number, this value can be changed depending how high you want to search for primes
     if i > 1:
         for j in range(2, i):
