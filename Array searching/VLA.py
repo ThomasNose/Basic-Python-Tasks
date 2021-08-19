080953ce90793f14d32cedfed95d33a19b446266
ThomasNose <thomasadugdale@hotmail.com>
2021-08-19 16:48:03 +0100

    Update VLA.py

--- Array searching/VLA.py
+++ Array searching/VLA.py
@@ -7,7 +7,7 @@ a = np.loadtxt("first_simple.txt")
 #ra = a[:,0]; dec = a[:,1]; peakflux = a[:,2]; totalflux = a[:,3]; minor = a[:,5]
 
 strong = a[a[:,2]>100] # sources > 100mJy
-strong_resolved = strong[strong[:,5]>0]
-strong_unresolved = strong[strong[:,5]==0]
+strong_resolved = strong[strong[:,5]>0] # Searching the 6th column where a source is resolved
+strong_unresolved = strong[strong[:,5]==0] # unresolved sources in 6th column
         
-print(time.time() - start_time)
+print(time.time() - start_time) # prints the time for code to run (useful for checking "for" loops against array indexing to see how much time is saved with different methods)
