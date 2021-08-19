1f1dbb2eeabc624cbf4309770fd8f88953a3f2d2
ThomasNose <thomasadugdale@hotmail.com>
2021-08-19 16:50:36 +0100

    Update Cart2Pol2Cart.py

--- Coordinate conversion/Cart2Pol2Cart.py
+++ Coordinate conversion/Cart2Pol2Cart.py
@@ -3,23 +3,23 @@ import numpy as np
 a = 1 + 1j
 b = 90
 
-def cart2pol(x, y):
+def cart2pol(x, y): # Basic function for converting cartestian coordinates to polar
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x)
-    return(rho, phi)
+    return(rho, phi) 
 
-def polar_rotation(b, phi):
+def polar_rotation(b, phi): # Applies a rotation to the polar coordinates of 90 degrees
     radian = np.radians(b)
     phi2 = phi + radian
     return(phi2)
 
-def pol2cart(rho, phi):
+def pol2cart(rho, phi): # Converts back to cartesian
     x = rho * np.cos(phi)
     y = rho * np.sin(phi)
     return(x, y)
 
 
 
-rho,phi = cart2pol(a.real, a.imag)
+rho,phi = cart2pol(a.real, a.imag) # .real and .imag separates the real and imaginary components of variable "a" which allows us to get an X and Y from a single variable
 p2 = polar_rotation(b,phi)
 x,y = pol2cart(rho, p2)
