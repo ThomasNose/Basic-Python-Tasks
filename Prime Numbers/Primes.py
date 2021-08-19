95e5c080dcc4adccac4ef903f305ac8cb1b57a80
ThomasNose <thomasadugdale@hotmail.com>
2021-08-19 16:52:47 +0100

    Update Primes.py

--- Prime Numbers/Primes.py
+++ Prime Numbers/Primes.py
@@ -1,7 +1,7 @@
 for i in range(0,3000): # 3000 is the maximum number, this value can be changed depending how high you want to search for primes
-    if i > 1:
-        for j in range(2, i):
-            if (i % j) == 0:
+    if i > 1: # 1 isn't a prime number :P 
+        for j in range(2, i): # Loop checking every number from 2-3000
+            if (i % j) == 0: # If any number in the sequence can be perfectly divided then then loop breaks and moves to the next number
                 break
         else:
-            print(i)
+            print(i) # Prints a prime number if previous condition isn't satisfied
