60166f60379fe6ecaefa257205b8d80e974d974d
ThomasNose <thomasadugdale@hotmail.com>
2021-08-19 16:42:06 +0100

    Add files via upload

---
+++ Array searching/Finding values in array.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+first = np.loadtxt("first_simple.txt")
+
+first = first[first[:,3]>100][:,:2] # Condition >100 in column 4 then take remain ing rows take first 2 columns
+
+decsort = np.sort(first[:,1]) # Take 2nd column then sort it
+
+decdiffs = decsort[1:] - decsort[:-1] # Cut out first and last one
+
+decdiffs = abs(decdiffs)
+
+mindiff = decdiffs.min()
+
+mindiff = np.argmin(decdiffs) # Tells us where min dec is
+
+#decsort[3266]
+#decsort[3267]
+
+dec0,dec1 = decsort[mindiff+1],decsort[mindiff]
+
+#dec0, dec1
+
+print(first[first[:,1]==dec0])
